bbb8d25f1d7c579b49b246e41eb45338ed019b01
s20115202 <109137115+s20115202@users.noreply.github.com>
2022-07-12 16:07:28 +0800

    v1

---
+++ script.js
@@ -0,0 +1,11 @@
+       // function change() {
+        //     document.body.style.background = document.getElementById("colors").value
+            
+        // }
+    <script>
+        document.getElementById("colors").addEventListener("change",function() {
+           
+        
+           document.body.style.background = document.getElementById("colors").value
+        })
+   </script>
